5e3d776bb857c3cca0b2ba292a1080ff9a6c50fe
Alex Delgado <delgadoa@echo.rutgers.edu>
2012-11-16 14:20:57 -0500

    refactored everything, added random tweet display functionality

--- js/append.js
+++ js/append.js
@@ -1,39 +1,48 @@
+function appendApproved(tweetToAppend, appendTo) {
+    var html;
+    
+    html = '<div class="tweet">'+
+             '<div class="approved-tweet">' +
+               '<div class="left-image pull-left">' +
+                 '<img src="' + tweetToAppend.image +'">' +
+               '</div>' +
+             '<div class="approved-information">' +
+               '<div class="user">' + tweetToAppend.username + '</div>' +
+               '<div class="individual-tweet">' + tweetToAppend.tweet + '</div>'+
+             '</div>';
+
+    appendTo.prepend(html);
+}
+
 
 function appendTweets(tweetsToAppend){
     var newTweets = tweetsToAppend["new-tweets"];
-    try{
-	tweetsToAppend = JSON.parse(newTweets);
-    }catch(error){
-	console.log("JSON Error");
-	return;
+
+    try {
+	
+	   tweetsToAppend = JSON.parse(newTweets);
+    
+    } catch(error) {
+    
+    	console.log("JSON Error");
+    	return;
+    
     }
-    for(var i = 0; i < tweetsToAppend.length; i++){
-	$('#tweets-content').prepend($('<div class="tweet">' +
-			    '<button class="btn btn-success approve-btn">Approve</button>' +
-			    '<div class="left-image pull-left"> ' +
-			    '<img src="' + tweetsToAppend[i].image + '"/>' +
-			    '</div>' +
-			    '<div class="individual-tweet">' +
-			    '<div class="user">' + tweetsToAppend[i].from +
-			    '</div>' +
-			    '<div class="tweet-content">' +
-			    tweetsToAppend[i].text +
-			    '</div>' +
-			    '</div>' +
-			       '</div>').fadeIn('fast'));
-    }    
+
+    var i = Math.floor( (Math.random() * tweetsToAppend.length) + 1 );
+
+    var html = '<div class="tweet">' +
+                 '<button class="btn btn-success approve-btn">Approve</button>' +
+                 '<div class="left-image pull-left">' +
+                    '<img src="' + tweetsToAppend[i].image + '">' +
+                 '</div>' +
+                 '<div class="individual-tweet">' +
+                   '<div class="user">' + tweetsToAppend[i].from + '</div>' +
+                   '<div class="tweet-content">' + tweetsToAppend[i].text + '</div>' +
+                 '</div>' +
+               '</div>';
+
+
+    $('#tweets-content').html(html);
 }
 
-function appendApproved(tweetToAppend, appendTo){
-    appendTo.prepend($('<div class="tweet">'+
-		       '<div class="approved-tweet">' +
-		       '<div class="left-image pull-left">' +
-		       '<img src="' + tweetToAppend.image +'"/>' +
-		       '</div>' +
-		       '<div class="approved-information">' +
-		       '<div class="user">' + tweetToAppend.username + '</div>' +
-		       '<div class="individual-tweet">' + tweetToAppend.tweet + '</div>'+
-		       '</div>').fadeIn('fast'));
-		       
-
-}
