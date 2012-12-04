75fb0ddd4de95cf04e205bd34411876a33b380ce
Jonathan Maltz <jm1271@gmail.com>
2012-12-04 12:17:25 -0500

    Tested for bad authorization and some other stuff.  Also fixed a bug where you could connect to incorrect db names

--- tests/testModel.js
+++ tests/testModel.js
@@ -3,7 +3,7 @@ var config = require('../lib/config.js').config;
 
 
 var model = new Model.model(config.mysql);
-model.connect(function(error, result){
+model.connect(function(error){
 	if(!error){
 		console.log('No error!!!');
 	}
@@ -13,8 +13,22 @@ model.connect(function(error, result){
 
 });
 
-/*var badModel = new Model.model(config.badMongo);
-badModel.connect(function(error, result){
+var badModel = new Model.model(config.badMysql);
+badModel.connect(function(error){
+	if(error){
 		console.log('Error is ' + error);
+	}
+	else{
+		console.log('Failure, the bad database still connected');
+	}
+});
 
-});*/
+var badAuth = new Model.model(config.badAuth);
+badAuth.connect(function(error){
+	if(error){
+		console.log('We got an error with the bad Auth ' + error);
+	}
+	else{
+		console.log('Successfully authed when we should not have');
+	}
+});
