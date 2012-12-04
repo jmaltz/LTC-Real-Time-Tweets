10665e7bead01ee87895691c0e2e3337ffff11f3
Jonathan Maltz <jm1271@gmail.com>
2012-12-03 22:19:33 -0500

    More stuff to make mysql happy

--- tests/testModel.js
+++ tests/testModel.js
@@ -2,16 +2,19 @@ var Model = require('../lib/tweetsModel.js');
 var config = require('../lib/config.js').config;
 
 
-var model = new Model.model(config.mongo);
+var model = new Model.model(config.mysql);
 model.connect(function(error, result){
-		if(!error){
-				console.log('No error!!!');
-		}
+	if(!error){
+		console.log('No error!!!');
+	}
+	else{
+		console.log('Error :( ' + error);
+	}
 
 });
 
-var badModel = new Model.model(config.badMongo);
+/*var badModel = new Model.model(config.badMongo);
 badModel.connect(function(error, result){
 		console.log('Error is ' + error);
 
-});
+});*/
