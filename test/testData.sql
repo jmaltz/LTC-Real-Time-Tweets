d1cd0eecfdb87e0cfeacd1125a0d6cb7b5f3e8b5
Jonathan Maltz <jm1271@gmail.com>
2012-12-27 23:47:41 -0500

    successfully removing by timestamp, now off to actually, you know, use that

--- test/testData.sql
+++ test/testData.sql
@@ -8,11 +8,17 @@ CREATE TABLE tweets(
 	PRIMARY KEY (id)
 );
 
-INSERT INTO tweets (timestamp, username, text, image, id) VALUES (0, 'negative one', 'Test #multiple', 'undefined', -1);
-INSERT INTO tweets (timestamp, username, text, image, id) VALUES (0, 'negative two', 'Test #multiple', 'undefined', -2);
-INSERT INTO tweets (timestamp, username, text, image, id) VALUES (0, 'negative three', 'Test #multiple', 'undefined', -3);
-INSERT INTO tweets (timestamp, username, text, image, id) VALUES (0, 'negative four', 'Test #unique_hashtag', 'undefined', -4);
-INSERT INTO tweets (timestamp, username, text, image, id) VALUES (0, 'negative six', 'Test #single', 'undefined', -6);
-INSERT INTO tweets (timestamp, username, text, image, id) VALUES (0, 'negative seven', 'Test #camelCase', 'undefined', -7);
-INSERT INTO tweets (timestamp, username, text, image, id) VALUES (0, 'negative eight', 'Test #camelcase', 'undefined', -8);
+INSERT INTO tweets (timestamp, username, text, image, id) VALUES ('2015-05-01 00:00:00', 'negative one', 'Test #multiple', 'undefined', -1);
+INSERT INTO tweets (timestamp, username, text, image, id) VALUES ('2015-05-01 00:00:00', 'negative two', 'Test #multiple', 'undefined', -2);
+INSERT INTO tweets (timestamp, username, text, image, id) VALUES ('2015-05-01 00:00:00', 'negative three', 'Test #multiple', 'undefined', -3);
+INSERT INTO tweets (timestamp, username, text, image, id) VALUES ('2015-05-01 00:00:00', 'negative four', 'Test #unique_hashtag', 'undefined', -4);
+INSERT INTO tweets (timestamp, username, text, image, id) VALUES ('2015-05-01 00:00:00', 'negative six', 'Test #single', 'undefined', -6);
+INSERT INTO tweets (timestamp, username, text, image, id) VALUES ('2015-05-01 00:00:00', 'negative seven', 'Test #camelCase', 'undefined', -7);
+INSERT INTO tweets (timestamp, username, text, image, id) VALUES ('2015-05-01 00:00:00', 'negative eight', 'Test #camelcase', 'undefined', -8);
+
+INSERT INTO tweets (timestamp, username, text, image, id) VALUES ('2012-05-01 00:00:00', 'negative ten', 'Test #timeStamp', 'undefined', -10);
+INSERT INTO tweets (timestamp, username, text, image, id) VALUES ('2012-06-02 00:00:00', 'negative eleven', 'Test #timeStamp', 'undefined', -11);
+INSERT INTO tweets (timestamp, username, text, image, id) VALUES ('2012-06-15 00:00:00', 'negative twelve', 'Test #timeStamp', 'undefined', -12);
+INSERT INTO tweets (timestamp, username, text, image, id) VALUES ('2012-06-25 00:00:00', 'negative thirteen', 'Test #timeStamp', 'undefined', -13);
+
 
--- test/testDelete.js
+++ test/testDelete.js
@@ -28,7 +28,181 @@ suite('Setup delete tests', function(){
 var runTests = function(){
 	
 	suite('Deletion Tests', function(){
+		deleteByHashtag();
+		deleteByTimestamp();
+	});
+}
+
+var deleteByTimestamp = function(){
+
+	suite('Delete by timestamp', function(){
+
+		var beforeEverything;
+		var inBetweenTweets;
+		var beforeNothing;
+		var equalToATweet;
 	
+		var nullTime;
+		var undefinedTime;
+		var wrongTypeString;
+		var wrongTypeInt;
+
+		var timestampHashtag;
+
+		var allRecords;
+		var lastThreeRecords;
+		var lastRecord;
+		var noRecords;
+		
+		setup(function(){
+			dbSetup.setup();
+			
+			beforeEverything = new Date(0);
+			inBetweenTweets = new Date(2012, 5, 1);
+			beforeNothing = new Date(2012, 6, 1);
+			equalToATweet = new Date(2012, 5, 15);
+
+			nullTime = null;
+			undefinedTime = undefined;
+			wrongTypeString = "HERPDERP";
+			wrongTypeInt = 12233;
+
+			timestampHashtag = 'timeStamp';
+
+			var tweetOne = {
+				id: -10
+				,username: 'negative ten'
+				,text: 'Test #timeStamp'
+				,image: 'undefined'
+			};
+			var tweetTwo = {
+				id: -11
+				,username: 'negative eleven'
+				,text: 'Test #timeStamp'
+				,image: 'undefined'
+			};
+			var tweetThree = {
+				id: -12
+				,username: 'negative twelve'
+				,text: 'Test #timeStamp'
+				,image: 'undefined'
+			};
+			var tweetFour = {
+				id: -13
+				,username: 'negative thirteen'
+				,text: 'Test #timeStamp'
+				,image: 'undefined'
+			};
+
+			allRecords = [tweetOne, tweetTwo, tweetThree, tweetFour];
+			lastThreeRecords = [tweetTwo, tweetThree, tweetFour];
+			lastRecord = [tweetFour];
+			noRecords = [];
+		});
+
+		test('Deleteing with a Date that is before all records should delete nothing', function(done){
+			
+			model.deletePriorTweets(beforeEverything, function(error, results){
+				assert.ifError(error);		
+				assert.equal(results.affectedRows, 0);
+				
+				model.getApprovedTweets(timestampHashtag, function(error, results){
+					
+					assert.ifError(error);
+					assert.deepEqual(results, allRecords);
+					done();
+				});
+			});
+		});
+
+		test('Deleting with a date that is in between records should keep all the records before it', function(done){
+			
+			model.deletePriorTweets(inBetweenTweets, function(error, results){
+				assert.ifError(error);
+				assert.equal(results.affectedRows, 1);
+	
+				model.getApprovedTweets(timestampHashtag, function(error, results){
+				
+					assert.ifError(error);
+					assert.deepEqual(lastThreeRecords, results);
+					done();
+				});	
+			});
+		});
+
+		test('Deleting should delete all tweets less than or equal to the time', function(done){
+			
+			model.deletePriorTweets(equalToATweet, function(error, results){
+				assert.ifError(error);
+				assert.equal(results.affectedRows, 3);
+	
+				model.getApprovedTweets(timestampHashtag, function(error, results){
+
+				assert.ifError(error);
+				assert.deepEqual(results, lastRecord);	
+				done();
+				});
+			});
+		});
+
+		test('Deleting after a given time should delete all tweets', function(done){
+
+			model.deletePriorTweets(beforeNothing, function(error, results){
+				assert.ifError(error);
+				assert.equal(results.affectedRows, 4);
+				
+				model.getApprovedTweets(timestampHashtag, function(error, results){
+					
+					assert.ifError(error);
+					assert.deepEqual(noRecords, results);
+					done();
+				});
+			});
+		});
+
+		test('Deleting with null time should give an error', function(done){
+			
+			model.deletePriorTweets(nullTime, function(error, results){
+
+				assert.ok(error);
+				done();
+			});
+		});
+	
+		test('Deleting with undefined time should give an error', function(done){
+
+			model.deletePriorTweets(undefinedTime, function(error, results){
+
+				assert.ok(error);
+				done();
+			});
+		});
+
+		test('Passing a string as the date should give an error', function(done){
+
+			model.deletePriorTweets(wrongTypeString, function(error, results){
+				
+				assert.ok(error);
+				done();
+			});
+		});
+
+		test('Passing an int as the date should give an error', function(done){
+
+			model.deletePriorTweets(wrongTypeInt, function(error, results){
+				
+				assert.ok(error);
+				done();
+			});
+		});
+
+	});
+
+}
+
+var deleteByHashtag = function(){
+
+	suite('Delete by hashtag', function(){	
 		var hashtagWithOneRecord;
 		var hashtagWithMultipleRecords;
 		var hashtagWithCamelCase;
@@ -133,6 +307,5 @@ var runTests = function(){
 				done();
 			});
 		});
-
 	});
-}			
+}
