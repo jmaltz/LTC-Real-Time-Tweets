7b5ff3c46cd3d622cc8f72ea636bb6b6864aefc0
Jonathan Maltz <jm1271@gmail.com>
2013-01-20 20:35:57 -0500

    added full database cleaning functionality and bumped package.json to 0.0.2

--- bin/startup.js
+++ bin/startup.js
@@ -1,12 +1,34 @@
-var router = require("../lib/router");
-var handlers = require("../lib/handlers");
-var server = require("../lib/server");
+var router = require('../lib/router')
+, handlers = require('../lib/handlers')
+, server = require('../lib/server')
+, Model = require('../lib/tweetsModel')
+, config = require('../lib/config').config
+, cleaner = require('../lib/dbCleaner');
+
 
 var requestHandlers = {};
-requestHandlers["/"] = handlers.index;
-requestHandlers["/index"] = handlers.index;
-requestHandlers["/admin"] = handlers.admin;
-requestHandlers["/js"] = handlers.asset;
-requestHandlers["/css"] = handlers.asset
+requestHandlers['/'] = handlers.index;
+requestHandlers['/index'] = handlers.index;
+requestHandlers['/admin'] = handlers.admin;
+requestHandlers['/js'] = handlers.asset;
+requestHandlers['/css'] = handlers.asset
 
 server.startServer(router, requestHandlers);
+
+var database = new Model.model(config.mysql);
+
+//connect to the database
+database.connect(function(error){
+	
+	if(error){
+		console.log('Error connecting to the database on startup ' + error);
+	}
+	else{
+		try{
+			var dbCleaner = new cleaner.DbCleaner(database, config.dbCleanOptions);
+		}
+		catch(error){
+			console.log('Error creating the database cleaning job ' + error);
+		}
+	}
+});
